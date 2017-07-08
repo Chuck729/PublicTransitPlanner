149c0d378dedc2a54a83642966d29863d0d089ea
Chuck729 <alexaca@rose-hulman.edu>
2017-07-08 01:00:44 -0400

    Fab starts new search activity/fragment and callback sends loc/dest from frag to activity

--- app/src/main/java/edu/rosehulman/alexaca/publictransitplanner/MainActivity.java
+++ app/src/main/java/edu/rosehulman/alexaca/publictransitplanner/MainActivity.java
@@ -3,7 +3,6 @@ package edu.rosehulman.alexaca.publictransitplanner;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -20,11 +19,11 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String username = intent.getStringExtra(LoginActivity.EXTRA_USERNAME);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        final Intent searchIntent = new Intent(this, SearchActivity.class);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(searchIntent);
             }
         });
     }
---
+++ app/src/main/java/edu/rosehulman/alexaca/publictransitplanner/SearchFragment.java
@@ -0,0 +1,123 @@
+package edu.rosehulman.alexaca.publictransitplanner;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link SearchFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link SearchFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SearchFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String Location = "location";
+    private static final String Destination = "destination";
+    private EditText locationET;
+    private EditText destinationET;
+
+    // TODO: Rename and change types of parameters
+    private String mLocation;
+    private String mDestination;
+
+    private OnButtonPressed mListener;
+
+    public SearchFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment SearchFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static SearchFragment newInstance(String param1, String param2) {
+        SearchFragment fragment = new SearchFragment();
+        Bundle args = new Bundle();
+        args.putString(Location, param1);
+        args.putString(Destination, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mLocation = getArguments().getString(Location);
+            mDestination = getArguments().getString(Destination);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        locationET = (EditText)view.findViewById(R.id.location);
+        destinationET = (EditText)view.findViewById(R.id.destination);
+        mLocation = locationET.getText().toString();
+        mDestination = destinationET.getText().toString();
+        Button directionsButton = (Button)view.findViewById(R.id.directions_button);
+        directionsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Press", "pressed");
+                mLocation = locationET.getText().toString();
+                mDestination = destinationET.getText().toString();
+                if (mListener != null) {
+                    if (mLocation.length() > 0 && mDestination.length() > 0)
+                        mListener.onGetDirectionsPressed(mLocation+";"+mDestination);
+                }
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnButtonPressed) {
+            mListener = (OnButtonPressed) context;
+        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnButtonPressed {
+        void onGetDirectionsPressed(String locAndDest);
+    }
+}
