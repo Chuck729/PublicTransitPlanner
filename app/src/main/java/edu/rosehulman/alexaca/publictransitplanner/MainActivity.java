e8f36979ca61a7986c705724f4b5ddae2a80bba5
Chuck729 <alexaca@rose-hulman.edu>
2017-07-28 00:37:19 -0400

    added map to main activity and current location button brings map to current location

--- app/src/main/java/edu/rosehulman/alexaca/publictransitplanner/MainActivity.java
+++ app/src/main/java/edu/rosehulman/alexaca/publictransitplanner/MainActivity.java
@@ -1,15 +1,29 @@
 package edu.rosehulman.alexaca.publictransitplanner;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private Location mLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +31,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        this.setTitle("test");
         Intent intent = getIntent();
-        String username = intent.getStringExtra(LoginActivity.EXTRA_NAME);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         final Intent searchIntent = new Intent(this, SearchActivity.class);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -29,6 +42,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
     @Override
@@ -45,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        switch(id) {
+        switch (id) {
             case R.id.action_settings:
                 break;
             case R.id.action_logout:
@@ -57,5 +73,33 @@ public class MainActivity extends AppCompatActivity {
 
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+
+            final LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+            mLocation = locationManager.getLastKnownLocation(locationManager.getBestProvider(new Criteria(), true));
+            LatLng latLang = new LatLng(mLocation.getLatitude(), mLocation.getLongitude());
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLang, 16));
+            FloatingActionButton myLocationFab = (FloatingActionButton)findViewById(R.id.my_location_fab);
+            myLocationFab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (ActivityCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                            || ActivityCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        mLocation = locationManager.getLastKnownLocation(locationManager.getBestProvider(new Criteria(), true));
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mLocation.getLatitude(), mLocation.getLongitude()), 16));
+                    }
+                }
+            });
+        }
+        
     }
 }
